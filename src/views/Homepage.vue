92495c63bd352716f58cf5404da871c701aceb79
delano <delano@onetimesecret.com>
2025-01-29 14:26:23 -0800

    [#596] 14:26

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
   import HomepagePlansCTA from '@/components/ctas/HomepagePlansCTA.vue';
   import HomepageTaglines from '@/components/HomepageTaglines.vue';
+  import HomepageLinksPlaceholder from '@/components/secrets/HomepageLinksPlaceholder.vue';
   import SecretForm from '@/components/secrets/form/SecretForm.vue';
   import { WindowService } from '@/services/window.service';
   import { computed } from 'vue';
@@ -12,22 +13,30 @@
 
 <template>
   <div class="container mx-auto min-w-[320px] max-w-2xl py-1">
-  <template v-if="!authenticated"
-                    class="mb-6" >
-    <div class="flex flex-col items-center text-center">
-      <h3 class="font-base mb-2 text-gray-600 dark:text-gray-100 sm:mx-12 sm:block md:mx-2">
-        {{ $t('web.homepage.tagline1') }}
-      </h3>
-      <p class="mx-12 mb-6 text-base italic text-gray-400 dark:text-gray-400 sm:mx-2">
-        {{ $t('web.homepage.tagline2') }}
-      </p>
-    </div>
-  </template>
+    <template
+      v-if="!authenticated"
+      class="mb-6">
+      <div class="flex flex-col items-center text-center">
+        <h3 class="font-base mb-2 text-gray-600 dark:text-gray-100 sm:mx-12 sm:block md:mx-2">
+          {{ $t('web.homepage.tagline1') }}
+        </h3>
+        <p class="mx-12 mb-6 text-base italic text-gray-400 dark:text-gray-400 sm:mx-2">
+          {{ $t('web.homepage.tagline2') }}
+        </p>
+      </div>
+    </template>
 
-    <SecretForm class="mb-8"
-                :with-recipient="false"
-                :with-asterisk="true"
-                :with-generate="true" />
+    <SecretForm
+      class="mb-8"
+      :with-recipient="false"
+      :with-asterisk="true"
+      :with-generate="true" />
+
+      <HomepageLinksPlaceholder
+        class="mb-16"
+        title="No secrets yet"
+        description="Create a secret above to get started."
+      />
 
   </div>
 </template>
