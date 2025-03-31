f400cd98326839c248dc0016d13d3f8c163d31b7
delano <delano@onetimesecret.com>
2025-03-31 13:03:02 -0700

    [#1182] Remove homepage tags from incoming template

--- src/views/secrets/IncomingSupportSecret.vue
+++ src/views/secrets/IncomingSupportSecret.vue
@@ -1,15 +1,10 @@
 <script setup lang="ts">
-import HomepageTaglines from '@/components/HomepageTaglines.vue';
-import InboundSecretForm from '@/components/secrets/form/InboundSecretForm.vue';
-import { WindowService } from '@/services/window.service';
-
-const authenticated = WindowService.get('authenticated') ?? false;
+  import InboundSecretForm from '@/components/secrets/form/InboundSecretForm.vue';
 
 </script>
 
 <template>
   <div class="rounded-lg bg-white p-6 shadow-md dark:bg-gray-800">
-    <HomepageTaglines v-if="authenticated" />
 
     <InboundSecretForm
       :enabled="true"
